e22560368698383ddf92993162f00e7d59e125cd
JoaoSant0s <ricardoelric@gmail.com>
2017-04-10 02:00:24 -0300

    Creating base project. Adding controller and motor car. Creating base tracks.

---
+++ Assets/Script/Car/CarController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour {
+
+    CarMotor carMotor;
+
+    void Start() {
+        carMotor = GetComponent<CarMotor>();
+    }    
+
+    void FixedUpdate() {
+        float steer = Input.GetAxis("Horizontal");
+        float acelerate = Input.GetAxis("Vertical");
+                
+        carMotor.RotateFrontWheels(steer);
+        carMotor.AcelerateCar(acelerate);            
+    }
+ 
+}
---
+++ Assets/Script/Car/CarMotor.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+                                
+public class CarMotor : MonoBehaviour {
+
+    [Header("Object balacing")]
+    [SerializeField]
+    Transform centerMass;
+
+    [Header("Controller values")]
+    [SerializeField]
+    float maxTorque;
+    
+    [Header("Wheels Details")]
+    [SerializeField]
+    WheelCollider[] wheelsColliders = new WheelCollider[4];
+    [SerializeField]
+    Transform[] wheelsGraphics = new Transform[4];
+
+    Rigidbody rb;
+
+    void Awake() {
+        rb = GetComponent<Rigidbody>();
+        rb.centerOfMass = centerMass.localPosition;
+    }
+
+    void Update() {
+        UpdateWheelsGraphic();
+    }   
+
+    void UpdateWheelsGraphic() {
+        for (int i = 0; i < wheelsColliders.Length; i++) {
+            wheelsGraphics[i].Rotate(0, -wheelsColliders[i].rpm / 60 * 360 * Time.deltaTime, 0);
+        }
+    }
+
+    internal void RotateFrontWheels(float steer) {
+        var finalSteer = steer * 45f;
+
+        wheelsColliders[2].steerAngle = finalSteer;
+        wheelsColliders[3].steerAngle = finalSteer;                
+    }
+
+    internal void AcelerateCar(float acelerate) {
+        var torque = maxTorque * acelerate;        
+
+        wheelsColliders[0].motorTorque = torque;
+        wheelsColliders[1].motorTorque = torque;        
+    }
+}
